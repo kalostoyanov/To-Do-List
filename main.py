8cf58048f9f80d31fdf80a3b9c86687ffb97af2c
Kaloyan Stoyanov <kalo.stoyanov@gmail.com>
2018-04-15 15:08:06 +0200

    Includes reading and writing from a file instead of relying solely on a temporary list.

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
 ## VARIABLES
-todoList = []
 newToDo = ""
 listIndex = 0
 deleteToDoValue = ""
 deleteChoice = ""
 
-## DEFINITIONS
+
+## OPEN AND READ FILE
+def read_file():  # NB: this works
+    global todoList
+    todoList = []
+    try:
+        with open("to_do_list.txt","r") as to_do_file:
+            todoList = to_do_file.read().splitlines()  # Split lines is necessary to avoid '\n' at the end of a str
+    except:
+        print("File not found. Creating a blank copy.")
+        open("to_do_list.txt","w").close()
+    return todoList
+
+
+def write_file():
+    global todoList
+    with open("to_do_list.txt","w") as to_do_file:
+        for i in todoList:
+            to_do_file.write(i + '\n')
+    return todoList
+
+
+## OPERATIONS
 # add new to do
 def addNewToDo(newtodo):
-	if newtodo in todoList:
-		print("This value is already added. Please use command 'print' to see your current to do list.")
-	else:
-		todoList.append(newtodo)
-		print("Added.")
+    read_file()
+    if newtodo in read_file():
+        print("This value is already added. Please use command 'print' to see your current to do list.")
+    else:
+        todoList.append(newtodo)
+        print("Added.")
+        write_file()
+
 
 # print to do list
 def printToDoList():
+    read_file()
     if todoList:
         print("These is your current to do list:")
         for listIndex in range(len(todoList)):
@@ -26,8 +51,9 @@ def printToDoList():
 
 # delete to do
 def deleteToDo():
+    read_file()
     if todoList:
-        printToDoList() # displays the current list for reference
+        printToDoList()  # displays the current list for reference
   
         # loop to check if user entered value / input
         deleteToDoChoice = None
@@ -47,8 +73,10 @@ def deleteToDo():
     else:
         print("Your list is empty. You can use \"add\" to populate it.")
 
+
 # delete to do value - subfunction
 def deleteToDoValue():
+    read_file()
     deleteToDoValue = input("Please enter the item you want to delete: ").title()
     #while (deleteToDoValue not in todoList) or (deleteToDoValue != "Exit"): # the two conditions cannot exist together
     while deleteToDoValue not in todoList:
@@ -58,9 +86,12 @@ def deleteToDoValue():
     else: # this is an else clause to the loop
             todoList.remove(deleteToDoValue)
             print("Deleted.")
+            write_file()
+
 
 # delete to do index - subfunction
 def deleteToDoIndex():
+    read_file()
     deleteToDoIndex = input("Please enter the position of the item you want to delete: ")
     deleteToDoIndexErrorCheck = True
     while deleteToDoIndexErrorCheck == True:
@@ -69,6 +100,7 @@ def deleteToDoIndex():
             del todoList[deleteToDoIndex]
             deleteToDoIndexErrorCheck = False
             print("Deleted.")
+            write_file()
             break
         except IndexError:
             deleteToDoIndex = input("This item does not exist. Please try again or use \"exit\" to abort: ")
@@ -79,8 +111,10 @@ def deleteToDoIndex():
             deleteToDoIndex = input("Please try again or use \"exit\" to abort: ")
             if deleteToDoIndex.title() == "Exit":
                 break
-                    
-                    
+
+
+## MAIN                 
+read_file()
 print("Please enter your command. Options: add, delete, print, exit.")
 command = input("").lower()
 while command != exit:
